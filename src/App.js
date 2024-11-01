a3bdd78ba24ba4ab286fd7ccf0ac23cec1eda1ba
eunicepang <34784157+eunicepang@users.noreply.github.com>
2024-10-31 22:15:45 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Flex, Input, ChakraProvider, Heading, UnorderedList, ListItem, Link  } from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box>
+      <ChakraProvider>
+        <Box
+          p={8} 
+          bg="purple.100" 
+          color="black" 
+          textAlign="center" 
+          borderRadius="md" 
+          maxW="100%" 
+          mx="auto" 
+          boxShadow="lg"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Heading as="h1" size="2xl" fontWeight="bold" mb={4}>
+            Eunice Pang
+          </Heading>
+        </Box>
+
+        <Box
+          p={10} 
+        >
+            <Heading>
+              Crowdfunder App
+            </Heading>
+            <UnorderedList>
+              <ListItem>Deployed to Sepolia Testnet</ListItem>
+              <ListItem>Transfers ETH from user address to smart contract</ListItem>
+              <ListItem>
+                <Link href="https://example.com" color="teal.500" isExternal>
+                  Link to Project
+                </Link>
+              </ListItem>
+              
+            </UnorderedList>
+        </Box>
+        <Box
+          p={10}
+        >
+            <Heading>
+              Staking App
+            </Heading>
+            <UnorderedList>
+              <ListItem>Deployed to Sepolia Testnet</ListItem>
+              <ListItem>Users stake ETH and receive RWD tokens based on ERC20 standard</ListItem>
+              <ListItem>
+                <Link href="https://example.com" color="teal.500" isExternal>
+                  Link to Project
+                </Link>
+              </ListItem>
+            </UnorderedList>
+          </Box>
+          <Box
+            p={10}
+          >
+            <Heading>
+              NFT Minting App
+            </Heading>
+            <UnorderedList>
+              <ListItem>Deployed to Sepolia Testnet</ListItem>
+              <ListItem>Users mint an NFT as a proof-of-attendance to events</ListItem>
+              <ListItem>
+                <Link href="https://example.com" color="teal.500" isExternal>
+                  Link to Project
+                </Link>
+              </ListItem>
+            </UnorderedList>
+          </Box>
+
+          <Box
+           p={8}>
+            <Flex direction="row" justify="center" align="center" gap="4" p="4" >
+        <Box w="100px" h="100px" >
+          <Link href="https://example.com" color="teal.500" isExternal>
+            Resume
+          </Link>
+        </Box>
+        <Box w="100px" h="100px" >LinkedIn</Box>
+        <Box w="100px" h="100px" >Item 3</Box>
+      </Flex>
+          </Box>
+      </ChakraProvider>
+
+      
+    </Box>
   );
 }
 
